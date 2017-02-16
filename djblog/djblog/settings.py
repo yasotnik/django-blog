6c4aa126f6621de18a531cdc5160285c8ba608ce
yasotnik <sotnik.edu@ya.ru>
2017-02-16 17:03:42 +0200

    Added Procfile + settings for Heroku

--- djblog/djblog/settings.py
+++ djblog/djblog/settings.py
@@ -157,3 +157,4 @@ STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
