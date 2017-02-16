cb6b463ae2cb7642c49ee5b51560090130c74e5e
yasotnik <sotnik.edu@ya.ru>
2017-02-16 17:16:29 +0200

    Changed settings

--- djblog/djblog/settings.py
+++ djblog/djblog/settings.py
@@ -158,6 +158,3 @@ STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
